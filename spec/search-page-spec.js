779637ac1e95af3a8c11b62a43878b4c2960a1a0
Yanmin <ymxing@thoughtworks.com>
2015-08-20 06:56:49 +0800

    [3MJ] Yanmin:add render view method in search page.

--- spec/search-page-spec.js
+++ spec/search-page-spec.js
@@ -1,8 +1,8 @@
-describe('search page', function(){
+describe('search page', function () {
     'use strict';
     it('should get correct user input and ' +
-    'do search when user click search button', function(){
-        var searchPage,search,fixture;
+    'do search when user click search button', function () {
+        var searchPage, search, fixture;
         fixture = setFixtures('<input value="mel" ' +
         'id="locationInput"/>');
         search = jasmine.createSpy('search');
@@ -12,6 +12,21 @@ describe('search page', function(){
 
         expect(searchPage.searchValue).toBe('mel');
         expect(search).toHaveBeenCalled();
-
     });
+    it('should render search results on page', function () {
+        var fixture, searchPage, data;
+        data = [
+            {
+                description: 'This is description',
+                name: 'Melbourne'
+            }
+        ];
+        fixture = setFixtures('<div id="results"></div>');
+        searchPage = new SearchPage();
+        searchPage.renderView(data);
+        expect(fixture.html()).toBe('<div id="results"><div class=' +
+        '"panel large-12 columns"><h5>Melbourne</h5>'+
+        '<h6>This is description</h6>'+
+        '<a href="#" class="like button tiny right">Like</a>'+
+        '</div></div>')})
 });
--- src/js/SearchPage.js
+++ src/js/SearchPage.js
@@ -3,7 +3,15 @@ function SearchPage() {
         this.searchValue = $("#locationInput").val();
         search(this.searchValue);
     };
-
+    this.renderView=function(data){
+        if(data.length === 0){return;}
+        $.each(data,function(i,location){
+            $("#results").append('<div class=' +
+            '"panel large-12 columns"><h5>'+location.name +
+            '</h5><h6>'+location.description +
+            '</h6><a href="#" class="like button tiny right">Like</a>'+
+            '</div>')})
+    };
 
 }
 
