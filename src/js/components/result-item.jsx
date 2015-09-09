75f2baea1e8c007bd867323427c50666c1fc0a74
Yanmin <ymxing@thoughtworks.com>
2015-09-09 23:59:30 +0800

    [3MJ] Yanmin:use react rewrite the function toggle like.

--- src/js/components/result-item.jsx
+++ src/js/components/result-item.jsx
@@ -1,13 +1,17 @@
 var React = require('react');
 
 module.exports = React.createClass({
-
+    handleChange: function(){
+        this.props.changeStatus(this.props.result.name);
+    },
     render: function(){
         return (
         <div className="panel large-12 columns">
             <h5>{this.props.result.name}</h5>
             <h6>{this.props.result.description}</h6>
-            <a href="#" className="like button tiny right">Like</a>
+            <a href="#" className="like button tiny right" onClick={this.handleChange}>
+                { this.props.result.status ? 'Liked' : 'Like' }
+            </a>
         </div>
         );
     }
--- src/js/components/results-list.jsx
+++ src/js/components/results-list.jsx
@@ -3,9 +3,19 @@ var _ = require('lodash');
 var ResultItem = require('./result-item.jsx');
 
 module.exports = React.createClass({
+
+    changeStatus: function(name){
+        var locations = this.props.results;
+        var location = _.first(_.where(locations, {name: name}));
+        location.status = !location.status;
+
+        this.props.parent.setState({results:locations});
+    },
+
     render: function () {
+        var self = this;
         var searchResults = _.map(this.props.results, function(result){
-            return <ResultItem result={result} />;
+            return <ResultItem result={result} changeStatus={self.changeStatus}/>;
         });
         return (
             <div id="searchResults" className="large-8 medium-8 columns">
